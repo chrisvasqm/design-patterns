87afd6ba4bb3dfda327d1d8023cd9809bcdebb19
chrisvasqm <christianvasquezmelo@gmail.com>
2022-03-05 12:00:36 -0400

    Solve Observer exercise

---
+++ src/com/chrisvasqm/exercises/observer/Observer.java
@@ -0,0 +1,5 @@
+package com.chrisvasqm.exercises.observer;
+
+public interface Observer {
+    void update();
+}
---
+++ src/com/chrisvasqm/exercises/observer/StockListView.java
@@ -0,0 +1,14 @@
+package com.chrisvasqm.exercises.observer;
+
+public class StockListView implements Observer {
+    private final DataSource dataSource;
+
+    public StockListView(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public void update() {
+        System.out.println("StockListView notified: " + dataSource.getValue());
+    }
+}
