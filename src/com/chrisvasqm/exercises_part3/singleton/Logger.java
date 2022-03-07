7db0f6817678e660c3dba85c5171cb403d7db63a
chrisvasqm <christianvasquezmelo@gmail.com>
2022-03-07 00:28:42 -0400

    Solve Singleton exercise

--- src/com/chrisvasqm/exercises_part3/singleton/Logger.java
+++ src/com/chrisvasqm/exercises_part3/singleton/Logger.java
@@ -2,9 +2,20 @@ package com.chrisvasqm.exercises_part3.singleton;
 
 public class Logger {
     private String fileName;
+    private final static Logger instance = new Logger();
 
-    public Logger(String fileName) {
+    private Logger() {}
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    public static Logger getInstance() {
+        return instance;
     }
 
     public void write(String message) {
